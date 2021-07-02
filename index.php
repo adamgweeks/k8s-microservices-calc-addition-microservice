c95da1eef5985c827b85fd8d18582c2a7360230f
Adam Weeks <adam.weeks@emc.com>
2021-07-02 17:56:06 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+<title>Don't speak to me directly!</title>
+</head>
+<body>
+<h2>Hey, I'm the addition microservice!</h2>
+<h3>Only the frontend microservice is supposed to talk to me directly!</h3>
+</body>
+</html>
